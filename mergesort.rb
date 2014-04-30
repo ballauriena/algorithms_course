b550d1360022e6ce05dac6c1c788de7ae67f22c5
ballauriena <lleecrowe@gmail.com>
2014-04-30 16:32:31 -0500

    Update mergesort.rb
    fixed indentation

--- mergesort.rb
+++ mergesort.rb
@@ -1,42 +1,42 @@
 class Mergesort
-	attr_accessor :array
+  attr_accessor :array
 
 
-	def initialize(array)
-		@array = array
+  def initialize(array)
+    @array = array
+  end
+
+
+  def round_up
+    return (@array.size)/2 + 1 if (@array.size) % 2 != 0
+    return (@array.size)/2
+  end
+
+
+  def split
+    nested_array = []
+    @array.each_slice(round_up){ |ary| nested_array << ary }
+    nested_array
+  end
+
+
+  def sort_array(ary, sorted = [])
+    return sorted if ary.size == 0
+
+    num = ary.pop 
+
+    if sorted.empty?
+      sorted << num
+    else
+      for i in 0..sorted.count
+	if num <= sorted[i] 
+	  sorted.insert(i, num)
+	  break
 	end
-
-
-	def round_up
-	    return (@array.size)/2 + 1 if (@array.size) % 2 != 0
-	    return (@array.size)/2
-	end
-
-
-	def split
-		nested_array = []
-		@array.each_slice(round_up){ |ary| nested_array << ary }
-		nested_array
-	end
-
-
-	def sort_array(ary, sorted = [])
-		return sorted if ary.size == 0
-
-		num = ary.pop 
-
-		if sorted.empty?
-			sorted << num
-		else
-			for i in 0..sorted.count
-				if num <= sorted[i] 
-					sorted.insert(i, num)
-					break
-				end
-			end
-		end
-		sort_array(ary, sorted)
-	end
+      end
+    end
+    sort_array(ary, sorted)
+  end
 
 
 end
