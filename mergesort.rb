ceff5285d693d01f51b1a19f38e23cf62fe4bb9b
Ballauriena <lleecrowe@gmail.com>
2014-05-02 14:45:58 -0500

    fixed merge conflict in mergesort.rb

--- mergesort.rb
+++ mergesort.rb
@@ -1,24 +1,16 @@
-class Mergesort
-  attr_accessor :array
-
-
-  def initialize(array)
-    @array = array
-  end
-
-
+class Array
+  
   def round_up
-    return (@array.size)/2 + 1 if (@array.size) % 2 != 0
-    return (@array.size)/2
-  end
+	    return (self.size)/2 + 1 if (self.size) % 2 != 0
+	    return (self.size)/2
+	end
 
 
-  def split
-    nested_array = []
-    @array.each_slice(round_up){ |ary| nested_array << ary }
-    nested_array
-  end
-
+	def split
+		nested_array = []
+		self.each_slice(round_up){ |ary| nested_array << ary }
+		nested_array
+	end
 
   def sort_array(ary, sorted = [])
     return sorted if ary.size == 0
@@ -29,14 +21,32 @@ class Mergesort
       sorted << num
     else
       for i in 0..sorted.count
-	if num <= sorted[i] 
-	  sorted.insert(i, num)
-	  break
-	end
+      	if num <= sorted[i] 
+      	  sorted.insert(i, num)
+      	  break
+      	end
       end
     end
     sort_array(ary, sorted)
   end
 
+  def merge
+    a = sort_array(split[0])
+    b = sort_array(split[1])
+    c = []
+    i = 0
+    j = 0
+    for k in 0..self.count
+      if a[i] < b[j] 
+        c[k] = a[i]
+        i += 1
+      else
+        c[k] = b[j]
+        j += 1
+      end
+    end
+    c
+  end
 
 end
+
--- mergesort_spec.rb
+++ mergesort_spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
 require_relative 'mergesort'
 
-describe Mergesort do 
+describe Array do 
 
+<<<<<<< HEAD
   let (:mergesort1) { Mergesort.new([7, 2, 9, 10, 6, 8, 1, 26]) }
   let (:mergesort2) { Mergesort.new([7, 2, 9, 10, 6, 8, 1, 26, 4]) }
 
@@ -50,3 +51,43 @@ describe Mergesort do
   end
 
 end
+=======
+	let (:array1) { [7, 2, 9, 10, 6, 8, 1, 26] }
+	let (:array2) { [7, 2, 9, 10, 6, 8, 1, 26, 4] }
+
+	describe "#round_up" do
+		it "returns an integer half the length of the array if the array is even" do
+			expect(array1.round_up).to eq(4)
+		end
+
+		it "rounds a float up to the nearest whole number if the array is an odd length" do
+			expect(array2.round_up).to eq(5)
+		end
+	end
+
+	describe "#split" do
+		it  "returns two arrays that are halves of the original array when the array is an even length" do
+			expect(array1.split).to eq([[7, 2, 9, 10], [6, 8, 1, 26]])
+		end
+
+		it  "returns two arrays that are halves of the original array when the array 
+			is an odd length with the first array containing one more element than the second" do
+			expect(array2.split).to eq([[7,2,9,10, 6], [8, 1, 26, 4]])
+		end
+	end
+
+	describe "#sort_array" do
+		it "sorts an array of numbers from least to greatest" do
+			arrays = array1.split
+			expect(array1.sort_array(arrays[0])).to eq([2, 7, 9, 10])
+		end
+	end
+
+  describe "#merge" do
+    it "merges the two half arrays in a sorted order" do
+      expect(array1.merge).to eq([1, 2, 6, 7, 8, 9, 10, 26])
+    end
+  end
+
+end
+>>>>>>> changed mergesort to an instance method for the array class
