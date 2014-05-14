0fe2bc19446c21d83e10493377346b21e623fb3d
ballauriena <lleecrowe@gmail.com>
2014-05-13 20:03:20 -0500

    Update mergesort_spec.rb
    fixed spacing

--- mergesort_spec.rb
+++ mergesort_spec.rb
@@ -3,35 +3,35 @@ require_relative 'mergesort'
 
 describe Array do 
 
-	let (:array1) { [7, 2, 9, 10, 6, 8, 1, 26] }
-	let (:array2) { [7, 2, 9, 10, 6, 8, 1, 26, 4] }
+  let (:array1) { [7, 2, 9, 10, 6, 8, 1, 26] }
+  let (:array2) { [7, 2, 9, 10, 6, 8, 1, 26, 4] }
 
-	describe "#round_up" do
-		it "returns an integer half the length of the array if the array is even" do
-			expect(array1.round_up).to eq(4)
-		end
+  describe "#round_up" do
+    it "returns an integer half the length of the array if the array is even" do
+      expect(array1.round_up).to eq(4)
+    end
 
-		it "rounds a float up to the nearest whole number if the array is an odd length" do
-			expect(array2.round_up).to eq(5)
-		end
-	end
+    it "rounds a float up to the nearest whole number if the array is an odd length" do
+      expect(array2.round_up).to eq(5)
+    end
+  end
 
-	describe "#split" do
-		it  "returns two arrays that are halves of the original array when the array is an even length" do
-			expect(array1.split).to eq([[7, 2, 9, 10], [6, 8, 1, 26]])
-		end
+  describe "#split" do
+    it  "returns two arrays that are halves of the original array when the array is an even length" do
+      expect(array1.split).to eq([[7, 2, 9, 10], [6, 8, 1, 26]])
+    end
 
-		it  "returns two arrays that are halves of the original array when the array 
-			is an odd length with the first array containing one more element than the second" do
-			expect(array2.split).to eq([[7,2,9,10, 6], [8, 1, 26, 4]])
-		end
-	end
+    it  "returns two arrays that are halves of the original array when the array 
+      is an odd length with the first array containing one more element than the second" do
+        expect(array2.split).to eq([[7,2,9,10, 6], [8, 1, 26, 4]])
+      end
+  end
 
-	describe "#sort_and_count_array" do
-		it "sorts an array of numbers from least to greatest" do
-			arrays = array1.split
-			expect(arrays[0].sort_and_count_array[1]).to eq([2, 7, 9, 10])
-		end
+   describe "#sort_and_count_array" do
+    it "sorts an array of numbers from least to greatest" do
+      arrays = array1.split
+      expect(arrays[0].sort_and_count_array[1]).to eq([2, 7, 9, 10])
+    end
 
     it "sorts an odd array of numbers from least to greatest" do
       arrays = array2.split
@@ -47,7 +47,7 @@ describe Array do
       arrays = array2.split
       expect(arrays[0].sort_and_count_array[0]).to eq(4)
     end
-	end
+  end
 
   describe "#merge_sort" do
     it "counts inversions in even arrays" do
