fb51b50c5ef186e7c148d2924b5d33aecb1c742f
ballauriena <lleecrowe@gmail.com>
2014-05-13 20:00:00 -0500

    Update mergesort_spec.rb
    added local changes manually due to a merge conflict...just easier this way for now

--- mergesort_spec.rb
+++ mergesort_spec.rb
@@ -3,49 +3,69 @@ require_relative 'mergesort'
 
 describe Array do 
 
-  let (:array1) { [7, 2, 9, 10, 6, 8, 1, 26] }
-  let (:array2) { [7, 2, 9, 10, 6, 8, 1, 26, 4] }
+	let (:array1) { [7, 2, 9, 10, 6, 8, 1, 26] }
+	let (:array2) { [7, 2, 9, 10, 6, 8, 1, 26, 4] }
 
-  describe "#round_up" do
-    it "returns an integer half the length of the array if the array is even" do
-      expect(array1.round_up).to eq(4)
-    end
+	describe "#round_up" do
+		it "returns an integer half the length of the array if the array is even" do
+			expect(array1.round_up).to eq(4)
+		end
 
-    it "rounds a float up to the nearest whole number if the array is an odd length" do
-      expect(array2.round_up).to eq(5)
-    end
-  end
+		it "rounds a float up to the nearest whole number if the array is an odd length" do
+			expect(array2.round_up).to eq(5)
+		end
+	end
 
-  describe "#split" do
-    it  "returns two arrays that are halves of the original array when the array is an even length" do
-      expect(array1.split).to eq([[7, 2, 9, 10], [6, 8, 1, 26]])
-    end
+	describe "#split" do
+		it  "returns two arrays that are halves of the original array when the array is an even length" do
+			expect(array1.split).to eq([[7, 2, 9, 10], [6, 8, 1, 26]])
+		end
 
-    it  "returns two arrays that are halves of the original array when the array 
-      is an odd length with the first array containing one more element than the second" do
-      expect(array2.split).to eq([[7,2,9,10, 6], [8, 1, 26, 4]])
-    end
-  end
+		it  "returns two arrays that are halves of the original array when the array 
+			is an odd length with the first array containing one more element than the second" do
+			expect(array2.split).to eq([[7,2,9,10, 6], [8, 1, 26, 4]])
+		end
+	end
 
-  describe "#sort_array" do
-    it "sorts an array of numbers from least to greatest" do
-      arrays = array1.split
-      expect(array1.sort_array(arrays[0])).to eq([2, 7, 9, 10])
-    end
+	describe "#sort_and_count_array" do
+		it "sorts an array of numbers from least to greatest" do
+			arrays = array1.split
+			expect(arrays[0].sort_and_count_array[1]).to eq([2, 7, 9, 10])
+		end
 
     it "sorts an odd array of numbers from least to greatest" do
       arrays = array2.split
-      expect(array2.sort_array(arrays[0])).to eq([2, 6, 7, 9, 10])
+      expect(arrays[0].sort_and_count_array[1]).to eq([2, 6, 7, 9, 10])
     end
-  end
+
+    it "counts the number of inversions in an even array" do
+      arrays = array1.split
+      expect(arrays[0].sort_and_count_array[0]).to eq(1)
+    end
+
+    it "counts the number of inversions in an odd arrray" do
+      arrays = array2.split
+      expect(arrays[0].sort_and_count_array[0]).to eq(4)
+    end
+	end
 
   describe "#merge_sort" do
-    it "merges the two half arrays in a sorted order" do
-      expect(array1.merge_sort).to eq([1, 2, 6, 7, 8, 9, 10, 26])
+    it "counts inversions in even arrays" do
+      expect(array1.merge_sort).to eq(12)
     end
 
-    it "merges the two uneven half arrays in a sorted order" do
-      expect(array2.merge_sort).to eq([1, 2, 4, 6, 7, 8, 9, 10, 26])
+    it "counts inversions in odd arrays" do
+      expect(array2.merge_sort).to eq(18)
+    end
+
+    it "returns the original array if it only contains one element" do
+      array = [6]
+      expect(array.merge_sort).to eq(0)
+    end
+
+    it "returns an empty array if the array is empty" do
+      array = []
+      expect(array.merge_sort).to eq(0)
     end
   end
 
